ccf6b2859792a03796df2aba97d08e8ccdd5bfa5
iAmAdamReid <adam@adamreid.me>
2018-10-05 08:30:06 -0700

    GET project, GET actions

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 
 // import db helpers
-const actionDb = require('./data/helpers/actionModel.js');
-const projectDb = require('./data/helpers/projectModel.js');
+const actionsDb = require('./data/helpers/actionModel.js');
+const projectsDb = require('./data/helpers/projectModel.js');
 
 const server = express();
 server.use(cors());
@@ -24,6 +24,16 @@ server.use(logger);
 /*** Actions Methods ***/
 
 //TODO: GET
+server.get('/api/actions', (req, res) => {
+    actionsDb.get()
+    .then(actions => {
+        return res.status(200).json(actions);
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json(({error: "Error retrieving actions."}))
+    })
+})
 
 //TODO: POST
 
@@ -35,12 +45,43 @@ server.use(logger);
 /*** Projects Methods ***/
 
 //TODO: GET
+server.get('/api/projects', (req, res) => {
+    projectsDb.get()
+    .then(projects => {
+        return res.status(200).json(projects);
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({error: "Error retrieving projects."})
+    })
+})
 
 //TODO: POST
 
 //TODO: DELETE
 
 //TODO: PUT
+
+
+
+
+// Design and build the necessary endpoints to:
+
+// perform CRUD operations on projects and actions.
+// retrieve the list of actions for a project.
+
+
+// Projects
+// id: number, no need to provide it when creating projects, the database will generate it.
+// name: string, up to 128 characters long, required.
+// description: string, no size limit, required.
+// completed: boolean to indicate if the project has been completed, not required
+// Actions
+// id: number, no need to provide it when creating posts, the database will automatically generate it.
+// project_id: number, required, must be the id of an existing project.
+// description: string, up to 128 characters long, required.
+// notes: string, no size limit, required. Used to record additional notes or requirements to complete the action.
+// completed: boolean to indicate if the action has been completed, not required
 
 
 
